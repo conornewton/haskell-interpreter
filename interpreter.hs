41d70d49fd6ebc922549a434b51ecdf59017177b
Conor Newton <conornewton@gmail.com>
2018-08-27 12:36:47 +0100

    maybe now uses monadic notation

--- interpreter.hs
+++ interpreter.hs
@@ -16,16 +16,14 @@ parseCommandDouble xs = readMaybe $ takeWhile isDigit xs
 
 parseCommand :: String -> Maybe Command
 parseCommand [] = Nothing
-parseCommand (x:'+':xs) = case parseCommand [x] of
-                              Just c1 -> case parseCommand xs of
-                                              Just c2 -> Just (Operation c2 Plus c2)
-                                              Nothing -> Nothing
-                              Nothing -> Nothing
-parseCommand (x:'*':xs) = case parseCommand [x] of
-                              Just c1 -> case parseCommand xs of
-                                              Just c2 -> Just (Operation c2 Multiply c2)
-                                              Nothing -> Nothing
-                              Nothing -> Nothing
+parseCommand (x:'+':xs) = do
+                            c1 <- parseCommand [x]
+                            c2 <- parseCommand xs
+                            Just (Operation c1 Plus c2)
+parseCommand (x:'*':xs) = do
+                            c1 <- parseCommand [x]
+                            c2 <- parseCommand xs
+                            Just (Operation c1 Multiply c2)
 parseCommand xs = case parseCommandDouble xs of
                     Just x -> Just (Number x)
                     Nothing -> Nothing
@@ -34,8 +32,8 @@ evalCommand :: Command -> Double
 evalCommand (Number x) = x
 evalCommand (Operation c1 op c2) = evalOp op (evalCommand c1)  (evalCommand c2)
     where evalOp :: Op -> Double -> Double -> Double
-          evalOp Plus x y= x + y
-          evalOp Multiply x y= x * y
+          evalOp Plus x y = x + y
+          evalOp Multiply x y = x * y
 
 main :: IO ()
 main = do
