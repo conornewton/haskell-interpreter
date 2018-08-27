4297e90aecb6c6f664c6ba1a75d93a2740a6b167
Conor Newton <conornewton@gmail.com>
2018-08-27 11:55:26 +0100

    now uses maybe for errors

--- interpreter.hs
+++ interpreter.hs
@@ -1,36 +1,47 @@
 import Data.Char
 import Text.Read (readMaybe)
 
-data Op = Plus | Multiply deriving (Show)
+data Op = Plus | Multiply
+          deriving (Show)
 
 
-data Command = Number Integer |
-               Operation Command Op Command |
-               Error deriving (Show)
+data Command = Number Double |
+               Operation Command Op Command
+               deriving (Show)
 
-parseCommandInteger :: String -> Maybe Integer
-parseCommandInteger [] = Nothing
-parseCommandInteger xs = readMaybe $ takeWhile isDigit xs
+parseCommandDouble :: String -> Maybe Double
+parseCommandDouble [] = Nothing
+parseCommandDouble xs = readMaybe $ takeWhile isDigit xs
 
 
-parseCommand :: String -> Command
-parseCommand [] = Error
-parseCommand (x:'+':xs) = Operation (parseCommand [x]) Plus (parseCommand xs)
-parseCommand (x:'*':xs) = Operation (parseCommand [x]) Multiply (parseCommand xs)
-parseCommand xs = case parseCommandInteger xs of
-                    Just x -> Number x
-                    Nothing -> Error
+parseCommand :: String -> Maybe Command
+parseCommand [] = Nothing
+parseCommand (x:'+':xs) = case parseCommand [x] of
+                              Just c1 -> case parseCommand xs of
+                                              Just c2 -> Just (Operation c2 Plus c2)
+                                              Nothing -> Nothing
+                              Nothing -> Nothing
+parseCommand (x:'*':xs) = case parseCommand [x] of
+                              Just c1 -> case parseCommand xs of
+                                              Just c2 -> Just (Operation c2 Multiply c2)
+                                              Nothing -> Nothing
+                              Nothing -> Nothing
+parseCommand xs = case parseCommandDouble xs of
+                    Just x -> Just (Number x)
+                    Nothing -> Nothing
 
-evalCommand :: Command -> Integer
+evalCommand :: Command -> Double
 evalCommand (Number x) = x
 evalCommand (Operation c1 op c2) = evalOp op (evalCommand c1)  (evalCommand c2)
-    where evalOp :: Op -> Integer -> Integer -> Integer
+    where evalOp :: Op -> Double -> Double -> Double
           evalOp Plus x y= x + y
           evalOp Multiply x y= x * y
 
 main :: IO ()
 main = do
         input <-getLine
-        putStrLn $ "=" ++ (show $ evalCommand $ parseCommand $ filter (\x -> not $ isSpace x) input)
+        case parseCommand $ filter (\x -> not $ isSpace x) input of
+            Just c -> putStrLn $ "=" ++ (show $ evalCommand c)
+            Nothing -> putStrLn "error"
         main
 
